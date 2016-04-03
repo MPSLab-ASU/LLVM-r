22c3ccb2a974d9578e0b83a64dfc6b2ecd4ebc79
whitequark <whitequark@whitequark.org>
2016-04-03 23:40:15 +0000

    [OR1K] Model flag, overflow flag and carry flag separately.

--- lib/Target/OR1K/OR1KISelLowering.cpp
+++ lib/Target/OR1K/OR1KISelLowering.cpp
@@ -99,9 +99,10 @@ OR1KTargetLowering::OR1KTargetLowering(const OR1KTargetMachine &TM,
   setOperationAction(ISD::SMUL_LOHI,           MVT::i32,   Expand);
   setOperationAction(ISD::SMUL_LOHI,           MVT::f32,   Expand);
 
-  // FIXME: implement (with l.addc etc)
-  setOperationAction(ISD::ADDC,                MVT::i32,   Expand);
-  setOperationAction(ISD::ADDE,                MVT::i32,   Expand);
+  if (!Subtarget.hasAddc()) {
+    setOperationAction(ISD::ADDC,              MVT::i32,   Expand);
+    setOperationAction(ISD::ADDE,              MVT::i32,   Expand);
+  }
   setOperationAction(ISD::SUBC,                MVT::i32,   Expand);
   setOperationAction(ISD::SUBE,                MVT::i32,   Expand);
 
